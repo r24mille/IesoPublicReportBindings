a23970c0891a960ba03844efab9c66debbbabfb6
Reid Miller <r24mille@uwaterloo.ca>
2013-11-11 00:26:09 -0500

    Initial commit of two public reports
    Initial commit which parses VGForecastSummary and
    GenOutputCapability.

---
+++ src/test/java/name/reidmiller/parseieso/client/GenOutputCapabilityClientTest.java
@@ -0,0 +1,48 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.theimo.schema.IMODocument;
+
+public class GenOutputCapabilityClientTest {
+	private ApplicationContext appContext;
+	private GenOutputCapabilityClient genOutputCapabilityClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		genOutputCapabilityClient = (GenOutputCapabilityClient) appContext
+				.getBean("genOutputCapabilityClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"GenOutputCapability did not unmaral an IMODocument",
+					genOutputCapabilityClient.unmarshal() instanceof IMODocument);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetIMODocHeader() {
+		assertNotNull("IMODocHeader could not be retrieved from XML",
+				genOutputCapabilityClient.getIMODocHeader());
+	}
+
+	@Test
+	public void testGetIMODocBody() {
+		assertNotNull("IMODocBody could not be retrieved from XML",
+				genOutputCapabilityClient.getIMODocBody());
+	}
+}
---
+++ src/test/java/name/reidmiller/parseieso/client/VGForecastSummaryClientTest.java
@@ -0,0 +1,48 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.schema.Document;
+
+public class VGForecastSummaryClientTest {
+	private ApplicationContext appContext;
+	private VGForecastSummaryClient vgForecastSummaryClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		vgForecastSummaryClient = (VGForecastSummaryClient) appContext
+				.getBean("vgForecastSummaryClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue("VGForecastSummary did not unmaral an Document",
+					vgForecastSummaryClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull("DocHeader could not be retrieved from XML",
+				vgForecastSummaryClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull("DocBody could not be retrieved from XML",
+				vgForecastSummaryClient.getDocBody());
+	}
+
+}
