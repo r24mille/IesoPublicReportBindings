a2f0a2646e6151e0a20379ebaa7bf83b76f32d60
Reid Miller <r24mille@uwaterloo.ca>
2013-12-20 22:37:08 -0500

    I missed the DayAheadShadowPricesClientTest in the previous commit.

---
+++ src/test/java/name/reidmiller/parseieso/client/DayAheadShadowPricesClientTest.java
@@ -0,0 +1,50 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.reports.schema.dashadowprices.Document;
+
+public class DayAheadShadowPricesClientTest {
+	private ApplicationContext appContext;
+	private DayAheadShadowPricesClient dayAheadShadowPricesClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		dayAheadShadowPricesClient = (DayAheadShadowPricesClient) appContext
+				.getBean("dayAheadShadowPricesClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"AdequacyClient did not unmaral a ca.ieso.reports.schema.dashadowprices.Document",
+					dayAheadShadowPricesClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull(
+				"ca.ieso.reports.schema.dashadowprices.DocHeader could not be retrieved from XML",
+				dayAheadShadowPricesClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull(
+				"ca.ieso.reports.schema.dashadowprices.DocBody could not be retrieved from XML",
+				dayAheadShadowPricesClient.getDocBody());
+	}
+}
