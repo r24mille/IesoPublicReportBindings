7049a8e8caa86a4b5f49d2805cbe0b5c567bb49d
Reid Miller <r24mille@uwaterloo.ca>
2013-12-23 08:46:30 -0600

    Added Historic Transmissions Outage and Planned Transmission Outage (Day) clients and related tests.

---
+++ src/test/java/name/reidmiller/parseieso/client/HistoricalTransmissionOutagesClientTest.java
@@ -0,0 +1,50 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.reports.schema.iomspublichistoricoutagehour.Document;
+
+public class HistoricalTransmissionOutagesClientTest {
+	private ApplicationContext appContext;
+	private HistoricalTransmissionOutagesClient historicalTransmissionOutagesClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		historicalTransmissionOutagesClient = (HistoricalTransmissionOutagesClient) appContext
+				.getBean("historicalTransmissionOutagesClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"AdequacyClient did not unmaral a ca.ieso.reports.schema.iomspublichistoricoutagehour.Document",
+					historicalTransmissionOutagesClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull(
+				"ca.ieso.reports.schema.iomspublichistoricoutagehour.DocHeader could not be retrieved from XML",
+				historicalTransmissionOutagesClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull(
+				"ca.ieso.reports.schema.iomspublichistoricoutagehour.DocBody could not be retrieved from XML",
+				historicalTransmissionOutagesClient.getDocBody());
+	}
+}
---
+++ src/test/java/name/reidmiller/parseieso/client/PlannedTransmissionOutagesDayClientTest.java
@@ -0,0 +1,50 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.reports.schema.iomspublicplannedoutageday.Document;
+
+public class PlannedTransmissionOutagesDayClientTest {
+	private ApplicationContext appContext;
+	private PlannedTransmissionOutagesDayClient plannedTransmissionOutagesDayClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		plannedTransmissionOutagesDayClient = (PlannedTransmissionOutagesDayClient) appContext
+				.getBean("plannedTransmissionOutagesDayClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"AdequacyClient did not unmaral a ca.ieso.reports.schema.iomspublicplannedoutageday.Document",
+					plannedTransmissionOutagesDayClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull(
+				"ca.ieso.reports.schema.iomspublicplannedoutageday.DocHeader could not be retrieved from XML",
+				plannedTransmissionOutagesDayClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull(
+				"ca.ieso.reports.schema.iomspublicplannedoutageday.DocBody could not be retrieved from XML",
+				plannedTransmissionOutagesDayClient.getDocBody());
+	}
+}
