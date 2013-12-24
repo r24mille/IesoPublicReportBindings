17a5fc9d68b49d924f8d215561e86ca63b8f5fc1
Reid Miller <r24mille@uwaterloo.ca>
2013-12-24 11:29:26 -0600

    Finished writing test cases for 10 new binding classes.

---
+++ src/test/java/name/reidmiller/parseieso/client/PredispatchIntertieSchedulingLimitsClientTest.java
@@ -0,0 +1,50 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.reports.schema.predispintertieschedlimits.Document;
+
+public class PredispatchIntertieSchedulingLimitsClientTest {
+	private ApplicationContext appContext;
+	private PredispatchIntertieSchedulingLimitsClient predispatchIntertieSchedulingLimitsClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		predispatchIntertieSchedulingLimitsClient = (PredispatchIntertieSchedulingLimitsClient) appContext
+				.getBean("predispatchIntertieSchedulingLimitsClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"Could not unmaral a ca.ieso.reports.schema.predispintertieschedlimits.Document",
+					predispatchIntertieSchedulingLimitsClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull(
+				"ca.ieso.reports.schema.predispintertieschedlimits.DocHeader could not be retrieved from XML",
+				predispatchIntertieSchedulingLimitsClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull(
+				"ca.ieso.reports.schema.predispintertieschedlimits.DocBody could not be retrieved from XML",
+				predispatchIntertieSchedulingLimitsClient.getDocBody());
+	}
+}
---
+++ src/test/java/name/reidmiller/parseieso/client/PredispatchMarketPriceClientTest.java
@@ -0,0 +1,47 @@
+package name.reidmiller.parseieso.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ca.ieso.reports.schema.predispmktprice.Document;
+
+public class PredispatchMarketPriceClientTest {
+	private ApplicationContext appContext;
+	private PredispatchMarketPriceClient predispatchMarketPriceClient;
+
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		predispatchMarketPriceClient = (PredispatchMarketPriceClient) appContext.getBean("predispatchMarketPriceClient");
+	}
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			assertTrue(
+					"Could not unmaral a ca.ieso.reports.schema.predispmktprice.Document",
+					predispatchMarketPriceClient.unmarshal() instanceof Document);
+		} catch (ClassCastException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetDocHeader() {
+		assertNotNull(
+				"ca.ieso.reports.schema.predispmktprice.DocHeader could not be retrieved from XML",
+				predispatchMarketPriceClient.getDocHeader());
+	}
+
+	@Test
+	public void testGetDocBody() {
+		assertNotNull(
+				"ca.ieso.reports.schema.predispmktprice.DocBody could not be retrieved from XML",
+				predispatchMarketPriceClient.getDocBody());
+	}
+}
